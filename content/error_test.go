f18ef405429137211b6838a8419c90187fcb920f
mstmdev <mstmdev@gmail.com>
2023-06-26 22:04:14 +0800

    Add more tests for the Error

--- content/error_test.go
+++ content/error_test.go
@@ -6,15 +6,24 @@ import (
 )
 
 func TestError_MarshalText(t *testing.T) {
-	cErr := NewError(io.EOF).(Error)
-	data, err := cErr.MarshalText()
-	if err != nil {
-		t.Errorf("Error.MarshalText error => %v", err)
-		return
+	testCases := []struct {
+		name   string
+		err    error
+		expect string
+	}{
+		{"with error", io.EOF, "EOF"},
+		{"with nil error", nil, ""},
 	}
-	expect := io.EOF.Error()
-	actual := string(data)
-	if expect != actual {
-		t.Errorf("test Error.MarshalText failed, expect to get %s, but actual get %s", expect, actual)
+	for _, tc := range testCases {
+		cErr := NewError(tc.err).(Error)
+		data, err := cErr.MarshalText()
+		if err != nil {
+			t.Errorf("Error.MarshalText error => %v", err)
+			return
+		}
+		actual := string(data)
+		if tc.expect != actual {
+			t.Errorf("test Error.MarshalText failed, expect to get %s, but actual get %s", tc.expect, actual)
+		}
 	}
 }
