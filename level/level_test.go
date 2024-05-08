e6387458f83fef2f9cd75b2b01697b04c5876c38
mstmdev <mstmdev@gmail.com>
2024-05-09 00:02:31 +0800

    Add the level.ParseLevel function (#32)

--- level/level_test.go
+++ level/level_test.go
@@ -36,3 +36,64 @@ func TestLevel(t *testing.T) {
 		})
 	}
 }
+
+func TestParseLevel(t *testing.T) {
+	testCases := []struct {
+		expect Level
+		text   string
+	}{
+		{DebugLevel, "DEBUG"},
+		{InfoLevel, "INFO"},
+		{WarnLevel, "WARN"},
+		{ErrorLevel, "ERROR"},
+		{NoneLevel, "NONE"},
+
+		{DebugLevel, "debug"},
+		{InfoLevel, "info"},
+		{WarnLevel, "warn"},
+		{ErrorLevel, "error"},
+		{NoneLevel, "none"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.text, func(t *testing.T) {
+			actual, err := ParseLevel(tc.text)
+			if err != nil {
+				t.Errorf("ParseLevel error, text=%s err=%s", tc.text, err)
+				return
+			}
+			if actual != tc.expect {
+				t.Errorf("ParseLevel error, [%s] expect:%d, actual:%d", tc.text, tc.expect, actual)
+			}
+		})
+	}
+}
+
+func TestParseLevel_ReturnError(t *testing.T) {
+	testCases := []struct {
+		expect Level
+		text   string
+	}{
+		{unknownLevel, "UNKNOWN"},
+		{unknownLevel, "INVALID"},
+		{unknownLevel, "HELLO"},
+
+		{unknownLevel, "unknown"},
+		{unknownLevel, "invalid"},
+		{unknownLevel, "hello"},
+		{unknownLevel, ""},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.text, func(t *testing.T) {
+			actual, err := ParseLevel(tc.text)
+			if err == nil {
+				t.Errorf("ParseLevel expect to get an error but get nil, text=%s", tc.text)
+				return
+			}
+			if actual != tc.expect {
+				t.Errorf("ParseLevel error, [%s] expect:%d, actual:%d", tc.text, tc.expect, actual)
+			}
+		})
+	}
+}
